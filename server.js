c9b18407543872719e72848755dadeacfade20ad
Fatih Acikkollu <f_acikkollu@hotmail.com>
2022-04-24 16:55:18 +0200

    Config for heroku cleanup

--- server.js
+++ server.js
@@ -2,12 +2,6 @@ const ws = require('ws');
 const http = require("http");
 const express = require("express");
 
-// if (process.argv.length < 3) {
-//     console.log("Usage: node app.js <port>");
-//     process.exit(1);
-// }
-
-const port = process.argv[2];
 const app = express();
 const server = http.createServer(app);
 const wss = new ws.WebSocket.Server({server});
